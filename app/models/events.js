fa68a076c0f6f2bd1127daf0c83e3fbd403f4309
caleybennett <cbennet7@conncoll.edu>
2020-01-07 10:16:35 -0500

    Events model
    Created event model and added user as owner, with fields of title,
    date and details.

---
+++ app/models/events.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema)
